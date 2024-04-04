e291586b43746ccc205a173b3cec418fc8b0468a
developers@continual.io <developers@continual.io>
2024-04-04 12:59:27 -0400

    nested object clears and unit test updates

--- eventFlows/continualProcessor/src/test/java/io/continual/services/processor/engine/library/filters/TimeFilterTest.java
+++ eventFlows/continualProcessor/src/test/java/io/continual/services/processor/engine/library/filters/TimeFilterTest.java
@@ -4,16 +4,8 @@ import org.json.JSONObject;
 import org.junit.Test;
 
 import io.continual.builder.Builder.BuildFailure;
-import io.continual.metrics.MetricsCatalog;
-import io.continual.metrics.impl.noop.NoopMetricsCatalog;
-import io.continual.services.processor.engine.model.Message;
+import io.continual.services.processor.engine.library.TestProcessingContext;
 import io.continual.services.processor.engine.model.MessageProcessingContext;
-import io.continual.services.processor.engine.model.Sink;
-import io.continual.services.processor.engine.model.Source;
-import io.continual.services.processor.engine.model.StreamProcessingContext;
-import io.continual.util.data.exprEval.ExprDataSource;
-import io.continual.util.data.exprEval.ExpressionEvaluator;
-import io.continual.util.data.json.JsonEval;
 import io.continual.util.time.Clock;
 import junit.framework.TestCase;
 
@@ -69,70 +61,6 @@ public class TimeFilterTest extends TestCase
 
 	private static MessageProcessingContext makeMessage ( JSONObject msgData )
 	{
-		final Message msg = Message.copyJsonToMessage ( msgData );
-		final MetricsCatalog metrics = new NoopMetricsCatalog ();
-		
-		return new MessageProcessingContext ()
-		{
-			@Override
-			public StreamProcessingContext getStreamProcessingContext () { return null; }
-
-			@Override
-			public String getId () { return "id"; }
-
-			@Override
-			public Message getMessage () { return msg; }
-
-			@Override
-			public boolean shouldContinue () { return true; }
-
-			@Override
-			public void stopProcessing () {}
-
-			@Override
-			public void warn ( String warningText ) {} 
-
-			@Override
-			public Source getSource ( String srcName ) { return null; }
-
-			@Override
-			public Sink getSink ( String sinkName ) { return null; }
-
-			@SuppressWarnings("unchecked")
-			@Override
-			public <T> T evalExpression ( String expression, Class<T> targetType, ExprDataSource... addlSrcs  )
-			{
-				final ExprDataSource eds = new ExprDataSource ()
-				{
-					@Override
-					public Object eval ( String label )
-					{
-						return JsonEval.eval ( msg.accessRawJson (), label );
-					}
-				};
-				final String asString = ExpressionEvaluator.evaluateText ( expression, eds );
-				if ( targetType.equals ( String.class ) )
-				{
-					return (T) asString;
-				}
-				if ( targetType.equals ( Long.class ) )
-				{
-					return (T) new Long ( Long.parseLong ( asString ) );
-				}
-				if ( targetType.equals ( Integer.class ) )
-				{
-					return (T) new Integer ( Integer.parseInt ( asString ) );
-				}
-				if ( targetType.equals ( Double.class ) )
-				{
-					return (T) new Double ( Double.parseDouble ( asString ) );
-				}
-
-				throw new IllegalArgumentException ( "Can't eval to " + targetType.getName () );
-			}
-
-			@Override
-			public MetricsCatalog getMetrics () { return metrics; }
-		};
+		return new TestProcessingContext ( msgData );
 	}
 }
