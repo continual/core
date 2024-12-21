a184c4be125567ca6e90fa575576766624fa0d76
developers@continual.io <developers@continual.io>
2024-12-21 18:12:55 -0500

    config load updates

--- eventFlows/processor/continualProcessor/src/main/java/io/continual/services/processor/config/readers/ConfigLoadContext.java
+++ eventFlows/processor/continualProcessor/src/main/java/io/continual/services/processor/config/readers/ConfigLoadContext.java
@@ -6,7 +6,15 @@ import io.continual.services.ServiceContainer;
 
 public interface ConfigLoadContext
 {
+	/**
+	 * Get the service container for this configuration load
+	 * @return a service container
+	 */
 	ServiceContainer getServiceContainer ();
 
+	/**
+	 * Get the search path packages used for this configuration load
+	 * @return a list of 0 or more search paths
+	 */
 	List<String> getSearchPathPackages ();
 }
